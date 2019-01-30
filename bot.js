1249a12f25b708ab727448f261b3dc9b3bfab664
Zₐₗgₒ <45021556+jayden8923@users.noreply.github.com>
2019-01-29 18:15:58 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,4 +4,11 @@ const config = require('./config/config.json');
 
 client.once('ready', () => {
 	console.log('Ready!');
+	
+	if (message.content.startsWith(`${prefix}help`)) {
+	message.channel.send('Pong.');
+} else if (message.content.includes(`@DR. Nope!#0908`)) {
+	message.channel.send('Dont ping me!');
+}
+
 });
