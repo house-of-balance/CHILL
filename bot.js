d37019007ef496936830ef67949feec606e2d35f
Zₐₗgₒ <45021556+jayden8923@users.noreply.github.com>
2019-01-29 18:18:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config/config.json');
+const { prefix, token } = require('./config/config.json');
 
 client.once('ready', () => {
 	console.log('Ready!');
