77b90f7d995f2c989d9e7731f94808fda3b0f1d3
Zₐₗgₒ <45021556+jayden8923@users.noreply.github.com>
2019-02-02 16:59:26 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,4 @@ const { prefix, token } = require('./config/config.json');
 
 client.once('ready', () => {
 	console.log('Ready!');
-	
-	if (message.content.startsWith(`${prefix}help`)) {
-	message.channel.send('Pong.');
-} else if (message.content.includes(`@DR. Nope!#0908`)) {
-	message.channel.send('Dont ping me!');
-}
-
 });
